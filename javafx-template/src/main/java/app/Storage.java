f9a3044d13b9647008a8e1a9fa5e17e1f6416c2e
andretan <andretan@stud.ntnu.no>
2022-09-18 02:17:42 +0200

    Made new Clothing-class, and made relevant changes

--- javafx-template/src/main/java/app/Storage.java
+++ javafx-template/src/main/java/app/Storage.java
@@ -1,78 +1,62 @@
 package app;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Storage {
 
-    private HashMap<Item, List<Object>>  storageList;
+    private HashMap<Clothing, Integer>  storageList;
 
     /* 
     Forutsetninger:
     - Tar utgangspunkt i at når man legger til et item i lageret definerer man pris også. På denne måten slipper brukeren
       å gå på oversiktssiden for å bestemme pris etter å ha lagt til et nytt item i lageret. Pris kan deretter oppdateres i 
-      oversiktssiden
+      oversiktssiden med tekstfelt  + knapp
 
     - Antar at tekstfeltet for å legge til eller fjerne antall items fra lager gjør nettopp dette, og at man ikke
       bestemmer et oppdatert antall i tekstfeltet.
-
-    Jeg må mest sannsynlig endre en del på denne klassen, siden jeg arbeidet uten en eksisterende Item-klasse. Men grunnlaget for klassen er implementert,
-    må bare oppdatere noen metode-navn og kanskje gjøre om til HashMap<Item, Integer>.
     */ 
-
-    /*
-    Mine forslag til attributter til Item-klassen:
-    - Navn (String)
-    - Merke (String)
-    - Størrelse (Char)
-    - Pris (Float)
-    - påSalg (boolean)
-    */
 
     public Storage() {
         this.storageList = new HashMap<>();
     }
 
-    public void addNewItem(Item item, int quantity, float price) {
-        if (!isItemInStorage(item) && isValidQuantity(quantity) && isValidPrice(price)) {
-            List<Object> list = new ArrayList<>(Arrays.asList(quantity, price));
-            this.storageList.put(item, list);
+    public void addNewClothing(Clothing clothing, int quantity) {
+        if (!isItemInStorage(clothing) && isValidQuantity(quantity)) {
+            this.storageList.put(clothing, quantity);
         } else {
             throw new IllegalStateException("This item is already in storage");
         }
     }
 
-    public void removeItem(Item item) {
-        if (!isItemInStorage(item)) {
+    public void removeItem(Clothing clothing) {
+        if (!isItemInStorage(clothing)) {
             throw new IllegalStateException("This item is not in storage");
         }
-        this.storageList.remove(item);
+        this.storageList.remove(clothing);
     }
 
-    public void IncreaseInventoryByOne(Item item) {
-        this.AddQuantity(item, 1);
+    public void IncreaseInventoryByOne(Clothing clothing) {
+        this.AddQuantity(clothing, 1);
     } 
 
-    public void DecreaseInventoryByOne(Item item) {
-        if (getQuantity(item) == 0) {
+    public void DecreaseInventoryByOne(Clothing clothing) {
+        if (getQuantity(clothing) == 0) {
             throw new IllegalStateException("You can not have negative quantity of item");
         }
-        this.removeQuantity(item, 1);
+        this.removeQuantity(clothing, 1);
     }
 
-    public void AddQuantity(Item item, int quantity) {
-        if (isItemInStorage(item) && isValidQuantity(quantity)) {
-            List<Object> list = this.getValueFromKey(item);
-            list.set(0, (Integer) list.get(0) + quantity);
+    public void AddQuantity(Clothing clothing, int quantity) {
+        if (isItemInStorage(clothing) && isValidQuantity(quantity)) {
+            this.storageList.put(clothing, this.getQuantity(clothing) + quantity);
         }
     }
 
-    public void removeQuantity(Item item, int quantity) {
-        if (isItemInStorage(item) && isValidQuantity(quantity)) {
-            List<Object> list = this.getValueFromKey(item);
-            list.set(0, (Integer) list.get(0) - quantity);
+    public void removeQuantity(Clothing clothing, int quantity) {
+        if (isItemInStorage(clothing) && isValidQuantity(quantity)) {
+            this.storageList.put(clothing, this.getQuantity(clothing) - quantity);
         }
     }
 
@@ -80,49 +64,52 @@ public class Storage {
     Bruk denne metoden for å få alle items på lager på en fin måte, sammen med antall varer. Brukes i Controller i ListView.
     Er på formatet:
 
-    Jacket: 5
-    Hat: 3
-    Socks: 8
+    Jacket, S: 5
+    Jeans, L: 3
+    Socks, M: 8
 
     */
 
     public List<String> homepageDisplay() {
         List<String> list = new ArrayList<>();
-        ArrayList<Item> keyList = new ArrayList<Item>(storageList.keySet());
+        ArrayList<Clothing> keyList = new ArrayList<Clothing>(storageList.keySet());
 
-        for (Item item : keyList) {
-            list.add(item.getTitle() + ": " + this.getQuantity(item));
+        for (Clothing clothing : keyList) {
+            list.add(clothing.getName() + ", " + clothing.getSize() + ": " + this.getQuantity(clothing));
         }
         return list;
     }
 
     /*
     Bruk denne metoden for å få alle items på lager på en fin måte, sammen med pris. Brukes i Controller i ListView.
-    Usikker på hvilket format det skal være...
+    Usikker på hvilket format det skal være på... 
     Mitt forslag:
 
     Socks
-        Brand: Nike
-        Sale: No
-        Price: 5kr
+        - Brand: Nike
+        - Size: M
+        - Sale: No
+        - Price: 5kr
     Jeans
-        Brand: Levi's
-        Sale: Yes
-        Price before: 10kr
-        Price now: 6kr
-        Total sale: 40%
+        - Brand: Levi's
+        - Size: L
+        - Sale: Yes
+        - Price before: 10kr
+        - Price now: 6kr
+        - Total sale: 40%
     
+    Blir dette for mye informasjon til et item?
     */
 
-    public String MarketDisplay() { // ikke ferdig metode, Item-klassen må bli ferdig først. Har bare kopiert fra homepageDisplay(). Er også usikker på format
+    public String MarketDisplay() { // ikke ferdig metode, er usikker på format
         String output = "";
-        ArrayList<Item> keyList = new ArrayList<Item>(storageList.keySet());
+        ArrayList<Clothing> keyList = new ArrayList<Clothing>(storageList.keySet());
 
-        for (Item item : keyList) {
-            if (item.equals(keyList.get(keyList.size()-1))) {
-                output += item.getTitle() + ": " + this.getQuantity(item);
+        for (Clothing clothing : keyList) {
+            if (clothing.equals(keyList.get(keyList.size()-1))) {
+                output += clothing.getName() + ": " + this.getQuantity(clothing);
             } else {
-                output += item.getTitle() + ": " + this.getQuantity(item) + "\n";
+                output += clothing.getName() + ": " + this.getQuantity(clothing) + "\n";
             }
         }
         return output;
@@ -135,42 +122,27 @@ public class Storage {
         return false;
     }
 
-    private boolean isValidPrice(float price) {
-        if (price >=0) {
+    private boolean isItemInStorage(Clothing clothing) {
+        if (this.storageList.containsKey(clothing)) {
             return true;
         }
         return false;
     }
 
-    private boolean isItemInStorage(Item item) {
-        if (this.storageList.containsKey(item)) {
-            return true;
-        }
-        return false;
-    }
-
-    public List<Object> getValueFromKey(Item item) {
-        return this.storageList.get(item);
-    }
-
-    public int getQuantity(Item item) {
-        return (Integer) this.getValueFromKey(item).get(0);
-    }
-
-    public int getPrice(Item item) {
-        return (Integer) this.getValueFromKey(item).get(1);
+    public int getQuantity(Clothing clothing) {
+        return this.storageList.get(clothing);
     }
 
     @Override
     public String toString() {
         String output = "";
-        ArrayList<Item> keyList = new ArrayList<Item>(storageList.keySet());
+        ArrayList<Clothing> keyList = new ArrayList<Clothing>(storageList.keySet());
 
-        for (Item item : keyList) {
-            if (item.equals(keyList.get(keyList.size()-1))) {
-                output += item.getTitle() + ", Antall: " + String.valueOf(storageList.get(item).get(0)) + ", Pris: " + String.valueOf(storageList.get(item).get(1)) + " kr";
+        for (Clothing clothing : keyList) {
+            if (clothing.equals(keyList.get(keyList.size()-1))) {
+                output += clothing + "\n" + "   - Quantity: " + String.valueOf(this.getQuantity(clothing));
             } else {
-                output += item.getTitle() + ", Antall: " + String.valueOf(storageList.get(item).get(0)) + ", Pris: " + String.valueOf(storageList.get(item).get(1)) + " kr" + "\n";
+                output += clothing + "\n" + "   - Quantity: " + String.valueOf(this.getQuantity(clothing)) + "\n";
             }
         }
         return output;
@@ -178,15 +150,15 @@ public class Storage {
 
     /*
     Små tester for oppførsel i main-metode
-    Alt funker som det skal
+    Alt funker som det skal utifra forutsetninger
     */
 
     public static void main(String[] args) {
         Storage storage = new Storage();
-        Item item = new Book("Nike sko", "Andreas");
-        Item item2 = new Book("Bukse", "Andreas");
-        storage.addNewItem(item, 2, 35);
-        storage.addNewItem(item2, 3, 60);
+        Clothing item = new Clothing("Bukse", "Levi's", 'M', 25);
+        Clothing item2 = new Clothing("Jakke", "Nike", 'S', 54.5);
+        storage.addNewClothing(item, 2);
+        storage.addNewClothing(item2, 3);
         System.out.println(storage);
         System.out.println("---------");
         storage.AddQuantity(item, 1);
@@ -204,5 +176,6 @@ public class Storage {
         storage.removeItem(item);
         System.out.println(storage);
         System.out.println("---------");
+        System.out.println(storage.homepageDisplay());
     }
 }
