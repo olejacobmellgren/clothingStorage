d32e32057b06302ad565af376861396007c71d45
Åshild Marie Magnussen Føllesdal <amfolle@stud.ntnu.no>
2022-09-18 17:48:23 +0200

    Fjernet ubrukt import

--- javafx-template/src/main/java/app/IFiles.java
+++ javafx-template/src/main/java/app/IFiles.java
@@ -2,7 +2,6 @@ package app;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
 
 public interface IFiles {
     public void writeToFile(String filename, Storage storage) throws FileNotFoundException;
