4adde21e1d7524716c2ca19a4aee41e9c1840bdb
andretan <andretan@stud.ntnu.no>
2022-10-12 12:15:28 +0200

    #33 removed comments in storage-class
    - also wrote javadoc for storageDisplay and priceDisplay

--- clothingStorage/core/src/main/java/clothingStorage/core/Storage.java
+++ clothingStorage/core/src/main/java/clothingStorage/core/Storage.java
@@ -12,11 +12,7 @@ public class Storage {
     private LinkedHashMap<Clothing, Integer>  storageList;
 
     /**
-     * Prerequisites:
-     *  - The price is set when an item is added to storage to prevent navigating to the overview page to set the price after the item has been added.
-     *    Subsequently, the price can be edited on the overview page with the text area and button.
-     * 
-     *  - Assume text area is used to add or remove a certain number of items from storage. The updated number of items is not determined in the text field.
+     * Initializes Storage object
      */
     public Storage() {
         this.storageList = new LinkedHashMap<>();
@@ -83,7 +79,7 @@ public class Storage {
      * Decreases number of clothing items
      * @param clothing item to be decreased
      * @param quantity number of clothing items to remove
-     * @throws IllegalStateException if number of clothing items if less than 0
+     * @throws IllegalStateException if number of clothing items is less than 0
      */
     public void decreaseQuantity(Clothing clothing, int quantity) {
         if (isClothingInStorage(clothing) && isValidQuantity(quantity) && getQuantity(clothing) - quantity >= 0 ) {
@@ -93,16 +89,11 @@ public class Storage {
         }
     }
 
-    /*
-    Bruk denne metoden for å få alle items på lager på en fin måte til hjemsiden, sammen med antall varer. Brukes i Controller i ListView.
-    Er på formatet:
-
-    Jacket, S: 5
-    Jeans, L: 3
-    Socks, M: 8
-
-    */
-
+    /**
+     * Iterates through all clothing items in storage and formats them to a string.
+     * Used to display clothes in storage-page 
+     * @return list of strings for every clothing in storage
+     */
     public List<String> storageDisplay() {
         List<String> list = new ArrayList<>();
         List<Clothing> keyList = new ArrayList<>(this.getAllClothes().keySet());
@@ -112,15 +103,11 @@ public class Storage {
         return list;
     }
 
-    /*
-    Bruk denne metoden for å få alle items på lager på en fin måte til oversiktssiden, sammen med pris. Brukes i Controller i ListView.
-    Er på formatet:
-
-    Jacket, Nike: 54,-
-    Socks, Adidas: 36,-
-    Jeans, Levi's: 60,-
-    */
-
+    /**
+     * Iterates through all clothing items and formats them to a string.
+     * Used to display clothes in price-page
+     * @return list of strings for every clothing in storage
+     */
     public List<String> priceDisplay() { 
         List<String> list = new ArrayList<>();
         List<Clothing> keyList = new ArrayList<>(this.getAllClothes().keySet());
@@ -201,49 +188,4 @@ public class Storage {
         }
         return output;
     }
-
-    /* Original tests to see if the functions work */
-    /* 
-    public static void main(String[] args) {
-        Storage storage = new Storage();
-        Clothing item = new Clothing("Bukse", "Levi's", 'M', 25);
-        Clothing item2 = new Clothing("Jakke", "Adidas", 'S', 54.5);
-        Clothing item3 = new Clothing("Jakke", "Nike", 'S', 100);
-        Clothing item4 = new Clothing("Sokker", "H&M", 'L', 122);
-
-        
-
-        
-        storage.addNewClothing(item, 2);
-        storage.addNewClothing(item2, 3);
-        storage.addNewClothing(item3, 6);
-        storage.addNewClothing(item4, 8);
-
-        System.out.println(storage.storageDisplay());
-
-        System.out.println(storage.getClothing(2));
-        System.out.println(item2.equals(item3));
-        System.out.println(item2.equals(storage.getClothing(0)));
-        
-        System.out.println(storage);
-        System.out.println("---------");
-        storage.increaseQuantity(item, 1);
-        System.out.println(storage);
-        System.out.println("---------");
-        storage.decreaseQuantity(item, 1);
-        System.out.println(storage);
-        System.out.println("---------");
-        storage.increaseQuantityByOne(item2);
-        System.out.println(storage);
-        System.out.println("---------");
-        storage.decreaseQuantityByOne(item2);
-        System.out.println(storage);
-        System.out.println("---------");
-        storage.removeClothing(item);
-        System.out.println(storage);
-        System.out.println("---------");
-        System.out.println(storage.storageDisplay());
-        System.out.println(storage.priceDisplay());
-    }
-    */
 }
