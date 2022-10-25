4bffa3ac338d4c4c0b1a3fe11754fb0449a7cef0
andretan <andretan@stud.ntnu.no>
2022-10-25 03:29:25 +0200

    #48 removed unused reports in storageTest
    What: removed two unused imports
    
    Why: codequality is now better since unused imports should not be listed

--- clothingStorage/core/src/test/java/clothingStorage/core/StorageTest.java
+++ clothingStorage/core/src/test/java/clothingStorage/core/StorageTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StorageTest {
     
     private Storage storage;
