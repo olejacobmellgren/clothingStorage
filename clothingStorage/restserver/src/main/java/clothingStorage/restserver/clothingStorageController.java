749af9a3d14209f5445faa39e1734adadea48fa8
Johanne Eide Omland <johanneeideomland@live.no>
2022-10-27 12:22:54 +0200

    #50 Imported packages
    What: Imported packages and functions needed
    
    Why: To handle HTTP requests
    
    Co-Authored-By: amfolle <amfolle@stud.ntnu.no>

--- clothingStorage/restserver/src/main/java/clothingStorage/restserver/clothingStorageController.java
+++ clothingStorage/restserver/src/main/java/clothingStorage/restserver/clothingStorageController.java
@@ -1,5 +1,37 @@
 package clothingStorage.restserver;
 
+import core.CardioSubCategory;
+import core.EntryManager;
+import core.ExerciseCategory;
+import core.LogEntry;
+import core.SortConfiguration;
+import core.StrengthSubCategory;
+import core.Subcategory;
+import math.Statistics;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@RestController
+@RequestMapping("/restapi")
 public class clothingStorageController {
     
 }
