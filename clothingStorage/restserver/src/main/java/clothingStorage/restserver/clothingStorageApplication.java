3e5383709558ca471d73f3310a685573e5a60c6f
Johanne Eide Omland <johanneeideomland@live.no>
2022-10-27 11:24:13 +0200

    What: Set up launcher for restserver
    Why: To launch server

--- clothingStorage/restserver/src/main/java/clothingStorage/restserver/clothingStorageApplication.java
+++ clothingStorage/restserver/src/main/java/clothingStorage/restserver/clothingStorageApplication.java
@@ -1,8 +1,27 @@
 package clothingStorage.restserver;
 
-/**
- * The StorageApplication.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * The clothingStorageApplication.
  */
-public class StorageApplication {
+public class clothingStorageApplication {
     
+
+    /**
+     * Starts spring server
+     * 
+     * @param string launches args
+     */
+    public static void main(String[] args){
+        SpringApplication.run(clothingStorageApplication, args);
+    }
+
+    /**
+     * Required for checkstyle to pass. Prevents springboot from breaking
+     */
+    public void dummy() {}
 }
