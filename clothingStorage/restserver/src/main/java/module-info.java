8fd629e33388393b1fc28c3d76b07ba95a3bdb7c
Johanne Eide Omland <johanneeideomland@live.no>
2022-11-07 11:00:32 +0100

    #50 Fixed pom file and module-info
    What: updated dependencies in pom file and added content to module-info
    
    Why: to import springboot packages
    
    Co-Authored-By; amfolle <amfolle@stud.ntnu.no>

--- clothingStorage/restserver/src/main/java/module-info.java
+++ clothingStorage/restserver/src/main/java/module-info.java
@@ -2,6 +2,14 @@ open module clothingStorage.restserver {
 
     requires transitive clothingStorage.json;
 
-    exports clothingStorage.restserver;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+
+    requires clothingStorage.core;
+
+    opens clothingStorage.restserver to spring.beans, spring.context, spring.web;
 }
 
