28f2101aae1935f71ad7620dec96d3d19b74a7ee
andretan <andretan@stud.ntnu.no>
2022-11-07 12:22:43 +0100

    #58 made some methods in client module
    What: added method for adding and removing clothing
    
    Why: Needed for the restserver
    
    Co-Authored-By: olejme <olejme@stud.ntnu.no>

--- clothingStorage/client/src/main/java/clothingStorage/client/LogClient.java
+++ clothingStorage/client/src/main/java/clothingStorage/client/LogClient.java
@@ -1,17 +1,24 @@
 package clothingStorage.client;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.json.JSONObject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import clothingStorage.core.Clothing;
+import clothingStorage.core.Storage;
+import clothingStorage.json.ClothingStoragePersistence;
 
 /**
  * The LogClient.
@@ -23,60 +30,154 @@ public class LogClient {
      * post
      * postAsync
      * getClothing
-     * getStorageHashMap
+     * getStorage
      * removeClothing
      * addClothing
      * updateClothing
      */
 
     /**
-     * OK response value.
+     * Port to the remote server.
      */
-    private static final int OK_CODE = 200;
-    /**
-     * URL to remote server.
-     */
-    private final String url;
+    private final URI endpointBaseUri;
+
     /**
      * Port to the remote server.
      */
-    private final int port;
+    private static final String APPLICATION_JSON = "application/json";
+
+    /**
+     * Port to the remote server.
+     */
+    private static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    /**
+     * Accept header.
+     */
+    private static final String ACCEPT_HEADER = "Accept";
+
+    /**
+     * Content-type header.
+     */
+    private static final String CONTENT_TYPE_HEADER = "Content-Type";
+
+    /**
+     * Objectmapper.
+     */
+    private ObjectMapper objectMapper;
+
+    /**
+     * Storage.
+     */
+    private Storage storage;
+
 
     /**
      * Constructs a LogClient from a builder.
+     * @throws URISyntaxException
      *
-     * @param serverUrl  server base url.
-     * @param serverPort server port.
      */
-    public LogClient(final String serverUrl, final int serverPort) {
-        this.url = serverUrl;
-        this.port = serverPort;
+    public LogClient() throws URISyntaxException {
+        this.endpointBaseUri = new URI("http://localhost:8080/clothingStorage");
+        objectMapper = ClothingStoragePersistence.createObjectMapper();
     }
 
-    public Clothing getClothing(final String id)
-            throws URISyntaxException, InterruptedException,
-            ExecutionException, ServerResponseException {
-        
-        HttpResponse<String> response = this.get("/api/v1/clothings/" + id);
-        String jsonString = response.body();
-        // hvordan ser jsonString ut???
-        JSONObject jsonObject = new JSONObject(jsonString);
+    public Storage getStorage() {
+        HttpRequest request = HttpRequest.newBuilder(endpointBaseUri)
+            .header(ACCEPT_HEADER, APPLICATION_JSON)
+            .GET()
+            .build();
+        try {
+            final HttpResponse<String> response = 
+                HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            this.storage = objectMapper.readValue(response.body(), Storage.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return storage;
+    }
 
-        String type = jsonObject.getString("type");
-        String brand = jsonObject.getString("brand");
-        char size = jsonObject.getString("size").charAt(0);
-        double price = Integer.parseInt(jsonObject.getString("price"));
-        double discount = Integer.parseInt(jsonObject.getString("discount"));
 
-        Clothing clothing = new Clothing(type, brand, size, price);
-        clothing.setDiscount(discount);
+
+
+    public Clothing getClothing(final String name) {
+        Clothing clothing;
+        HttpRequest request = HttpRequest.newBuilder(endpointBaseUri.resolve("/clothes/" + name))
+            .header(ACCEPT_HEADER, APPLICATION_JSON)
+            .GET()
+            .build();
+        try {
+            final HttpResponse<String> response = 
+                HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            clothing = objectMapper.readValue(response.body(), Clothing.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return clothing;
     }
 
-    public void addClothing(Clothing clothing)
-            throws URISyntaxException, InterruptedException,
-            ExecutionException, ServerResponseException {
+    public boolean addClothing(Clothing clothing, int quantity) throws JsonProcessingException {
+        Boolean added;
+        String json = objectMapper.writeValueAsString(clothing);
+        System.out.println(json);
         
+        HttpRequest request = HttpRequest.newBuilder(endpointBaseUri.resolve("/clothes/" + clothing.getName()))
+          .header(ACCEPT_HEADER, APPLICATION_JSON)
+          .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+          .PUT(BodyPublishers.ofString(json))
+          .build();
+        try {
+            final HttpResponse<String> response =
+                HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            added = objectMapper.readValue(response.body(), Boolean.class);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return added;
+    }
+
+    public boolean removeClothing(Clothing clothing) {
+        Boolean removed;
+        try {
+            HttpRequest request = HttpRequest.newBuilder(endpointBaseUri.resolve("/clothes/" + clothing.getName()))
+                .header(ACCEPT_HEADER, APPLICATION_JSON)
+                .DELETE()
+                .build();
+            final HttpResponse<String> response =
+                HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            removed = objectMapper.readValue(response.body(), Boolean.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return removed;
+    }
+
+    public boolean updateClothing(Clothing clothing) throws JsonProcessingException {
+        Boolean updated;
+        String json = objectMapper.writeValueAsString(clothing);
+        
+        HttpRequest request = HttpRequest.newBuilder(endpointBaseUri.resolve("/clothes/" + clothing.getName()))
+          .header(ACCEPT_HEADER, APPLICATION_JSON)
+          .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+          .PUT(BodyPublishers.ofString(json))
+          .build();
+        try {
+            final HttpResponse<String> response =
+                HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            updated = objectMapper.readValue(response.body(), Boolean.class);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return updated;
+    }
+
+    public static void main(String[] args) throws URISyntaxException, JsonProcessingException {
+        Clothing clothing = new Clothing("Pants", "Nike", 'M', 43);
+        LogClient logClient = new LogClient();
+        logClient.addClothing(clothing, 5);
         
     }
+
 }
